3c03684712b447f120069121c6c82f5ae91082c2
tacypoc <tacypoc@users.noreply.github.com>
2016-11-21 00:01:56 +0900

    Add namespace

--- include/Vector.hpp
+++ include/Vector.hpp
@@ -1,4 +1,11 @@
 #ifndef UUID_E2494444_AF31_11E6_9A65_0800274CD854
 #define UUID_E2494444_AF31_11E6_9A65_0800274CD854
 
+namespace dicek
+{
+  namespace math
+  {
+  }
+}
+
 #endif /* UUID_E2494444_AF31_11E6_9A65_0800274CD854 */
