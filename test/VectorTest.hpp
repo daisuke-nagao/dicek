ce706993f74e958c8ee9ace45fefa85c93edfa1e
tacypoc <tacypoc@users.noreply.github.com>
2016-11-26 23:53:15 +0900

    Add assertion

--- test/VectorTest.hpp
+++ test/VectorTest.hpp
@@ -1,4 +1,5 @@
 #include <cxxtest/TestSuite.h>
+#include <typeinfo>
 #include "dicek/vector.hpp"
 
 class VectorTest : public CxxTest::TestSuite {
@@ -16,18 +17,20 @@ public:
     void testInstanceTraits( void )
     {
         using namespace dicek::math;
-        vector<3, double, traits > v;
+        vector<3, double, traits> v;
     }
 
     void testDIM( void )
     {
         using namespace dicek::math;
         static_assert( vector<3, double>::DIM == 3, "vector<3,double>::DIM is not 3" );
+        TS_ASSERT_EQUALS( 3, ( vector<3, double>::DIM ) );
     }
 
     void test_real_type( void )
     {
         using namespace dicek::math;
         static_assert( std::is_same< vector<3, double>::real_type, double>::value, "vector<3, double>::real_type is not double" );
+        TS_ASSERT( typeid( vector<3, double>::real_type ) == typeid( double ) );
     }
 };
