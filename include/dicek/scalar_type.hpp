38904dc00f13c0b60e0c52d55a4cf37a322ac0d1
tacypoc <tacypoc@users.noreply.github.com>
2016-12-03 17:17:05 +0900

    Add test_const_type

--- include/dicek/scalar_type.hpp
+++ include/dicek/scalar_type.hpp
@@ -1,13 +1,15 @@
 #ifndef UUID_DFDD573E_B92E_11E6_AB3C_0800274CD854
 #define UUID_DFDD573E_B92E_11E6_AB3C_0800274CD854
 
+#include <type_traits>
+
 namespace dicek
 {
     namespace math
     {
         template<typename T>
         struct scalar_type {
-            typedef T type;
+            typedef typename std::remove_const<T>::type type;
         };
     }
 }
--- test/scalar_typeTest.hpp
+++ test/scalar_typeTest.hpp
@@ -16,6 +16,12 @@ public:
         static_assert( std::is_same<double, dicek::math::scalar_type<double>::type>::value, "two types are not the same" );
         TS_ASSERT( ( std::is_same<double, dicek::math::scalar_type<double>::type>::value ) );
     }
+
+    void test_const_type( void )
+    {
+        static_assert( std::is_same<float, dicek::math::scalar_type<const float>::type>::value, "two types are not the same" );
+        TS_ASSERT( ( std::is_same<float, dicek::math::scalar_type<const float>::type>::value ) );
+    }
 };
 
 #endif /* UUID_A8A43C38_B929_11E6_AB3C_0800274CD854 */
