cf613aa100052fed65d19720b3eabb0af21f75ed
Daisuke Nagao <daisuke-nagao@users.noreply.github.com>
2022-02-08 00:52:05 +0900

    Apply clang-format-12

--- test/scalar_traitsTest.hpp
+++ test/scalar_traitsTest.hpp
@@ -25,50 +25,45 @@ SOFTWARE.
 #define UUID_A8A43C38_B929_11E6_AB3C_0800274CD854
 
 #include <cxxtest/TestSuite.h>
+
 #include <type_traits>
 
 #include "dicek/scalar_traits.hpp"
 
 class scalar_traitsTest : public CxxTest::TestSuite {
-public:
-    void test_plain_type( void )
-    {
-        static_assert( std::is_same<float, dicek::math::scalar_traits<float>::type>::value, "two types are not the same" );
-        TS_ASSERT( ( std::is_same<float, dicek::math::scalar_traits<float>::type>::value ) );
+ public:
+  void test_plain_type(void) {
+    static_assert(std::is_same<float, dicek::math::scalar_traits<float>::type>::value, "two types are not the same");
+    TS_ASSERT((std::is_same<float, dicek::math::scalar_traits<float>::type>::value));
 
-        static_assert( std::is_same<double, dicek::math::scalar_traits<double>::type>::value, "two types are not the same" );
-        TS_ASSERT( ( std::is_same<double, dicek::math::scalar_traits<double>::type>::value ) );
-    }
+    static_assert(std::is_same<double, dicek::math::scalar_traits<double>::type>::value, "two types are not the same");
+    TS_ASSERT((std::is_same<double, dicek::math::scalar_traits<double>::type>::value));
+  }
 
-    void test_const_type( void )
-    {
-        static_assert( std::is_same<float, dicek::math::scalar_traits<const float>::type>::value, "two types are not the same" );
-        TS_ASSERT( ( std::is_same<float, dicek::math::scalar_traits<const float>::type>::value ) );
-    }
+  void test_const_type(void) {
+    static_assert(std::is_same<float, dicek::math::scalar_traits<const float>::type>::value, "two types are not the same");
+    TS_ASSERT((std::is_same<float, dicek::math::scalar_traits<const float>::type>::value));
+  }
 
-    void test_volatile_type( void )
-    {
-        static_assert( std::is_same<float, dicek::math::scalar_traits<volatile float>::type>::value, "two types are not the same" );
-        TS_ASSERT( ( std::is_same<float, dicek::math::scalar_traits<volatile float>::type>::value ) );
-    }
+  void test_volatile_type(void) {
+    static_assert(std::is_same<float, dicek::math::scalar_traits<volatile float>::type>::value, "two types are not the same");
+    TS_ASSERT((std::is_same<float, dicek::math::scalar_traits<volatile float>::type>::value));
+  }
 
-    void test_reference_type( void )
-    {
-        static_assert( std::is_same<float, dicek::math::scalar_traits<float&>::type>::value, "two types are not the same" );
-        TS_ASSERT( ( std::is_same<float, dicek::math::scalar_traits<float&>::type>::value ) );
-    }
+  void test_reference_type(void) {
+    static_assert(std::is_same<float, dicek::math::scalar_traits<float&>::type>::value, "two types are not the same");
+    TS_ASSERT((std::is_same<float, dicek::math::scalar_traits<float&>::type>::value));
+  }
 
-    void test_conj( void )
-    {
-        TS_ASSERT_EQUALS( dicek::math::scalar_traits<float>::conj( 1.5 ), 1.5f );
-        TS_ASSERT_EQUALS( dicek::math::scalar_traits<std::complex<double>>::conj( std::complex<double>( 1.5, -2 ) ), std::complex<double>( 1.5, 2 ) );
-    }
+  void test_conj(void) {
+    TS_ASSERT_EQUALS(dicek::math::scalar_traits<float>::conj(1.5), 1.5f);
+    TS_ASSERT_EQUALS(dicek::math::scalar_traits<std::complex<double>>::conj(std::complex<double>(1.5, -2)), std::complex<double>(1.5, 2));
+  }
 
-    void test_abs( void )
-    {
-        TS_ASSERT_EQUALS( dicek::math::scalar_traits<float>::abs( -1.5f ), 1.5f );
-        TS_ASSERT_EQUALS( dicek::math::scalar_traits<std::complex<double>>::abs( std::complex<double>( -1.5f, 2.0f ) ), sqrt( 1.5*1.5 + 4 ) );
-    }
+  void test_abs(void) {
+    TS_ASSERT_EQUALS(dicek::math::scalar_traits<float>::abs(-1.5f), 1.5f);
+    TS_ASSERT_EQUALS(dicek::math::scalar_traits<std::complex<double>>::abs(std::complex<double>(-1.5f, 2.0f)), sqrt(1.5 * 1.5 + 4));
+  }
 };
 
 #endif /* UUID_A8A43C38_B929_11E6_AB3C_0800274CD854 */
--- test/vector2cTest.hpp
+++ test/vector2cTest.hpp
@@ -25,191 +25,180 @@ SOFTWARE.
 #define UUID_3A12F814_B92D_11E6_AB3C_0800274CD854
 
 #include <cxxtest/TestSuite.h>
-#include <typeinfo>
-#include <complex>
+
 #include <cfloat>
+#include <complex>
+#include <typeinfo>
+
 #include "dicek/linalg/vector.hpp"
 
 class vector2cTest : public CxxTest::TestSuite {
-public:
-    using complexf = std::complex<float>;
-    using vector2c = dicek::math::linalg::vector<3, complexf>;
+ public:
+  using complexf = std::complex<float>;
+  using vector2c = dicek::math::linalg::vector<3, complexf>;
 
-    void testDIM( void )
-    {
-        static_assert( vector2c::DIM == 3, "vector<3,complexf>::DIM is not 3" );
-        TSM_ASSERT_EQUALS( "vector<3,complexf>::DIM is not 3", 3, ( vector2c::DIM ) );
-    }
+  void testDIM(void) {
+    static_assert(vector2c::DIM == 3, "vector<3,complexf>::DIM is not 3");
+    TSM_ASSERT_EQUALS("vector<3,complexf>::DIM is not 3", 3, (vector2c::DIM));
+  }
 
-    void test_scalar_type( void )
-    {
-        static_assert( std::is_same< vector2c::scalar_type, complexf>::value, "vector2c::scalar_type is not complexf" );
-        TSM_ASSERT( "vector2c::scalar_type is not complexf", typeid( vector2c::scalar_type ) == typeid( complexf ) );
-    }
+  void test_scalar_type(void) {
+    static_assert(std::is_same<vector2c::scalar_type, complexf>::value, "vector2c::scalar_type is not complexf");
+    TSM_ASSERT("vector2c::scalar_type is not complexf", typeid(vector2c::scalar_type) == typeid(complexf));
+  }
 
-    void test_default_constructor( void )
-    {
-        vector2c v;
-        TSM_ASSERT_EQUALS( "Not zero", v[ 0 ], complexf() );
-        TSM_ASSERT_EQUALS( "Not zero", v[ 1 ], complexf() );
-        TSM_ASSERT_EQUALS( "Not zero", v[ 2 ], complexf() );
-    }
+  void test_default_constructor(void) {
+    vector2c v;
+    TSM_ASSERT_EQUALS("Not zero", v[0], complexf());
+    TSM_ASSERT_EQUALS("Not zero", v[1], complexf());
+    TSM_ASSERT_EQUALS("Not zero", v[2], complexf());
+  }
 
-    void test_at( void )
-    {
-        vector2c v;
+  void test_at(void) {
+    vector2c v;
 
-        v.at( 0 ) = complexf( 5, 1 );
-        v.at( 1 ) = complexf( 7, 2 );
-        v.at( 2 ) = complexf( 11, 3 );
+    v.at(0) = complexf(5, 1);
+    v.at(1) = complexf(7, 2);
+    v.at(2) = complexf(11, 3);
 
-        TSM_ASSERT_EQUALS( "at(0) is not (5, 1)", v.at( 0 ), complexf( 5, 1 ) );
-        TSM_ASSERT_EQUALS( "at(1) is not (7, 2)", v.at( 1 ), complexf( 7, 2 ) );
-        TSM_ASSERT_EQUALS( "at(2) is not (11, 3)", v.at( 2 ), complexf( 11, 3 ) );
-    }
+    TSM_ASSERT_EQUALS("at(0) is not (5, 1)", v.at(0), complexf(5, 1));
+    TSM_ASSERT_EQUALS("at(1) is not (7, 2)", v.at(1), complexf(7, 2));
+    TSM_ASSERT_EQUALS("at(2) is not (11, 3)", v.at(2), complexf(11, 3));
+  }
 
-    void test_at_const( void )
-    {
-        vector2c v;
+  void test_at_const(void) {
+    vector2c v;
 
-        v.at( 0 ) = complexf( 5, 1 );
-        v.at( 1 ) = complexf( 7, 2 );
-        v.at( 2 ) = complexf( 11, 3 );
+    v.at(0) = complexf(5, 1);
+    v.at(1) = complexf(7, 2);
+    v.at(2) = complexf(11, 3);
 
-        const auto& ref = v;
+    const auto& ref = v;
 
-        TSM_ASSERT_EQUALS( "at(0) is not (5, 1)", v.at( 0 ), complexf( 5, 1 ) );
-        TSM_ASSERT_EQUALS( "at(1) is not (7, 2)", v.at( 1 ), complexf( 7, 2 ) );
-        TSM_ASSERT_EQUALS( "at(2) is not (11, 3)", v.at( 2 ), complexf( 11, 3 ) );
-    }
+    TSM_ASSERT_EQUALS("at(0) is not (5, 1)", v.at(0), complexf(5, 1));
+    TSM_ASSERT_EQUALS("at(1) is not (7, 2)", v.at(1), complexf(7, 2));
+    TSM_ASSERT_EQUALS("at(2) is not (11, 3)", v.at(2), complexf(11, 3));
+  }
 
-    void test_at_throw( void )
-    {
-        vector2c v;
-        TSM_ASSERT_THROWS( "Exception not thrown", v.at( 3 ), std::out_of_range );
-        TSM_ASSERT_THROWS( "Exception not thrown", const_cast<const decltype( v )&>( v ).at( 3 ), std::out_of_range );
-    }
+  void test_at_throw(void) {
+    vector2c v;
+    TSM_ASSERT_THROWS("Exception not thrown", v.at(3), std::out_of_range);
+    TSM_ASSERT_THROWS("Exception not thrown", const_cast<const decltype(v)&>(v).at(3), std::out_of_range);
+  }
 
-    void test_operator_square_bracket( void )
-    {
-        vector2c v;
-        v[ 0 ] = complexf( 5, 1 );
-        v[ 1 ] = complexf( 7, 2 );
-        v[ 2 ] = complexf( 11, 3 );
+  void test_operator_square_bracket(void) {
+    vector2c v;
+    v[0] = complexf(5, 1);
+    v[1] = complexf(7, 2);
+    v[2] = complexf(11, 3);
 
-        TSM_ASSERT_EQUALS( "operator[](0) is not 5 + i", v[ 0 ], complexf( 5, 1 ) );
-        TSM_ASSERT_EQUALS( "operator[](1) is not 7 + 2i", v[ 1 ], complexf( 7, 2 ) );
-        TSM_ASSERT_EQUALS( "operator[](2) is not 11 + 3i", v[ 2 ], complexf( 11, 3 ) );
-    }
+    TSM_ASSERT_EQUALS("operator[](0) is not 5 + i", v[0], complexf(5, 1));
+    TSM_ASSERT_EQUALS("operator[](1) is not 7 + 2i", v[1], complexf(7, 2));
+    TSM_ASSERT_EQUALS("operator[](2) is not 11 + 3i", v[2], complexf(11, 3));
+  }
 
-    void test_operator_square_bracket_const( void )
-    {
-        vector2c v;
-        v[ 0 ] = complexf( 5, 1 );
-        v[ 1 ] = complexf( 7, 2 );
-        v[ 2 ] = complexf( 11, 3 );
+  void test_operator_square_bracket_const(void) {
+    vector2c v;
+    v[0] = complexf(5, 1);
+    v[1] = complexf(7, 2);
+    v[2] = complexf(11, 3);
 
-        const auto& ref = v;
+    const auto& ref = v;
 
-        TSM_ASSERT_EQUALS( "operator[](0) is not 5 + i", ref[ 0 ], complexf( 5, 1 ) );
-        TSM_ASSERT_EQUALS( "operator[](1) is not 7 + 2i", ref[ 1 ], complexf( 7, 2 ) );
-        TSM_ASSERT_EQUALS( "operator[](2) is not 11 + 3i", ref[ 2 ], complexf( 11, 3 ) );
-    }
+    TSM_ASSERT_EQUALS("operator[](0) is not 5 + i", ref[0], complexf(5, 1));
+    TSM_ASSERT_EQUALS("operator[](1) is not 7 + 2i", ref[1], complexf(7, 2));
+    TSM_ASSERT_EQUALS("operator[](2) is not 11 + 3i", ref[2], complexf(11, 3));
+  }
 
-    void test_operator_add( void )
-    {
-        vector2c v, w;
-        v[ 0 ] = 1;
-        v[ 1 ] = 2;
-        v[ 2 ] = 3;
+  void test_operator_add(void) {
+    vector2c v, w;
+    v[0] = 1;
+    v[1] = 2;
+    v[2] = 3;
 
-        w[ 0 ] = complexf( 0, 10 );
-        w[ 1 ] = complexf( 0, 20 );
-        w[ 2 ] = complexf( 0, 30 );
+    w[0] = complexf(0, 10);
+    w[1] = complexf(0, 20);
+    w[2] = complexf(0, 30);
 
-        const auto& _v = v;
-        const auto& _w = w;
+    const auto& _v = v;
+    const auto& _w = w;
 
-        vector2c x = _v + _w;
-        vector2c y = _w + _v;
+    vector2c x = _v + _w;
+    vector2c y = _w + _v;
 
-        TSM_ASSERT_EQUALS( "operator+ is not 11", x[ 0 ], complexf( 1, 10 ) );
-        TSM_ASSERT_EQUALS( "operator+ is not 22", x[ 1 ], complexf( 2, 20 ) );
-        TSM_ASSERT_EQUALS( "operator+ is not 33", x[ 2 ], complexf( 3, 30 ) );
+    TSM_ASSERT_EQUALS("operator+ is not 11", x[0], complexf(1, 10));
+    TSM_ASSERT_EQUALS("operator+ is not 22", x[1], complexf(2, 20));
+    TSM_ASSERT_EQUALS("operator+ is not 33", x[2], complexf(3, 30));
 
-        TSM_ASSERT_EQUALS( "operator+ is not 11", y[ 0 ], x[ 0 ] );
-        TSM_ASSERT_EQUALS( "operator+ is not 22", y[ 1 ], x[ 1 ] );
-        TSM_ASSERT_EQUALS( "operator+ is not 33", y[ 2 ], x[ 2 ] );
-    }
+    TSM_ASSERT_EQUALS("operator+ is not 11", y[0], x[0]);
+    TSM_ASSERT_EQUALS("operator+ is not 22", y[1], x[1]);
+    TSM_ASSERT_EQUALS("operator+ is not 33", y[2], x[2]);
+  }
 
-    void test_scale( void )
-    {
-        vector2c v;
-        v[ 0 ] = complexf( 1, 10 );
-        v[ 1 ] = complexf( 2, 20 );
-        v[ 2 ] = complexf( 3, 30 );
+  void test_scale(void) {
+    vector2c v;
+    v[0] = complexf(1, 10);
+    v[1] = complexf(2, 20);
+    v[2] = complexf(3, 30);
 
-        vector2c w = v.scale( 3 );
-        TS_ASSERT_EQUALS( w[ 0 ], complexf( 3, 30 ) );
-        TS_ASSERT_EQUALS( w[ 1 ], complexf( 6, 60 ) );
-        TS_ASSERT_EQUALS( w[ 2 ], complexf( 9, 90 ) );
-    }
+    vector2c w = v.scale(3);
+    TS_ASSERT_EQUALS(w[0], complexf(3, 30));
+    TS_ASSERT_EQUALS(w[1], complexf(6, 60));
+    TS_ASSERT_EQUALS(w[2], complexf(9, 90));
+  }
 
-    void test_map( void )
-    {
-        vector2c v;
-        v[ 0 ] = complexf( 1, 10 );
-        v[ 1 ] = complexf( 2, 20 );
-        v[ 2 ] = complexf( 3, 30 );
+  void test_map(void) {
+    vector2c v;
+    v[0] = complexf(1, 10);
+    v[1] = complexf(2, 20);
+    v[2] = complexf(3, 30);
 
-        const auto& _v = v;
+    const auto& _v = v;
 
-        vector2c w = _v.map( []( complexf val ) { return val*val; } );
-        TS_ASSERT_EQUALS( w[ 0 ], complexf( 1, 10 ) * complexf( 1, 10 ) );
-        TS_ASSERT_EQUALS( w[ 1 ], complexf( 2, 20 ) * complexf( 2, 20 ) );
-        TS_ASSERT_EQUALS( w[ 2 ], complexf( 3, 30 ) * complexf( 3, 30 ) );
+    vector2c w = _v.map([](complexf val) { return val * val; });
+    TS_ASSERT_EQUALS(w[0], complexf(1, 10) * complexf(1, 10));
+    TS_ASSERT_EQUALS(w[1], complexf(2, 20) * complexf(2, 20));
+    TS_ASSERT_EQUALS(w[2], complexf(3, 30) * complexf(3, 30));
 
-        vector2c x = _v.map( []( complexf val ) -> complexf { return val + 2.0f; } );
-        TS_ASSERT_EQUALS( x[ 0 ], complexf( 3, 10 ) );
-        TS_ASSERT_EQUALS( x[ 1 ], complexf( 4, 20 ) );
-        TS_ASSERT_EQUALS( x[ 2 ], complexf( 5, 30 ) );
-    }
+    vector2c x = _v.map([](complexf val) -> complexf { return val + 2.0f; });
+    TS_ASSERT_EQUALS(x[0], complexf(3, 10));
+    TS_ASSERT_EQUALS(x[1], complexf(4, 20));
+    TS_ASSERT_EQUALS(x[2], complexf(5, 30));
+  }
 
-    void test_inner_product( void )
-    {
-        vector2c v, w;
-        v[ 0 ] = complexf( 1, 4 );
-        v[ 1 ] = complexf( 2, 5 );
-        v[ 2 ] = complexf( 3, 6 );
+  void test_inner_product(void) {
+    vector2c v, w;
+    v[0] = complexf(1, 4);
+    v[1] = complexf(2, 5);
+    v[2] = complexf(3, 6);
 
-        w[ 0 ] = complexf( 2, 3 );
-        w[ 1 ] = complexf( 5, 7 );
-        w[ 2 ] = complexf( 11, 13 );
+    w[0] = complexf(2, 3);
+    w[1] = complexf(5, 7);
+    w[2] = complexf(11, 13);
 
-        const auto& _v = v;
-        const auto& _w = w;
+    const auto& _v = v;
+    const auto& _w = w;
 
-        vector2c::scalar_type x = inner_product( _v, _w );
+    vector2c::scalar_type x = inner_product(_v, _w);
 
-        TS_ASSERT_EQUALS( x, complexf( 1, 4 ) * std::conj( complexf( 2, 3 ) ) + complexf( 2, 5 ) * std::conj( complexf( 5, 7 ) ) + complexf( 3, 6 ) *std::conj( complexf( 11, 13 ) ) );
-    }
+    TS_ASSERT_EQUALS(x, complexf(1, 4) * std::conj(complexf(2, 3)) + complexf(2, 5) * std::conj(complexf(5, 7)) + complexf(3, 6) * std::conj(complexf(11, 13)));
+  }
 
-    void test_norm( void )
-    {
-        vector2c v;
-        v[ 0 ] = complexf( 1, -1 );
-        v[ 1 ] = complexf( 1, -1 );
-        v[ 2 ] = complexf( 1, -1 );
-        double expect;
+  void test_norm(void) {
+    vector2c v;
+    v[0] = complexf(1, -1);
+    v[1] = complexf(1, -1);
+    v[2] = complexf(1, -1);
+    double expect;
 
-        double n = norm( v, 2.0 );
-        expect = sqrt( pow( abs( v[ 0 ] ), 2 ) + pow( abs( v[ 1 ] ), 2 ) + pow( abs( v[ 2 ] ), 2 ) );
-        TS_ASSERT_DELTA( expect, n, expect * FLT_EPSILON );
+    double n = norm(v, 2.0);
+    expect   = sqrt(pow(abs(v[0]), 2) + pow(abs(v[1]), 2) + pow(abs(v[2]), 2));
+    TS_ASSERT_DELTA(expect, n, expect * FLT_EPSILON);
 
-        n = norm( v, 3.0l );
-        expect = pow( pow( abs( v[ 0 ] ), 3 ) + pow( abs( v[ 1 ] ), 3 ) + pow( abs( v[ 2 ] ), 3 ), 1.0 / 3 );
-        TS_ASSERT_DELTA( expect, n, expect * FLT_EPSILON );
-    }
+    n      = norm(v, 3.0l);
+    expect = pow(pow(abs(v[0]), 3) + pow(abs(v[1]), 3) + pow(abs(v[2]), 3), 1.0 / 3);
+    TS_ASSERT_DELTA(expect, n, expect * FLT_EPSILON);
+  }
 };
 
 #endif /* UUID_3A12F814_B92D_11E6_AB3C_0800274CD854 */
