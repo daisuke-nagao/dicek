cf5583d9835eb8e97d8085c99516e741e0de8ce5
Daisuke Nagao <daisuke-nagao@users.noreply.github.com>
2022-02-08 01:31:48 +0900

    Apply clang-format-12

--- include/dicek/linalg/vector.hpp
+++ include/dicek/linalg/vector.hpp
@@ -25,114 +25,100 @@ SOFTWARE.
 #define UUID_E2494444_AF31_11E6_9A65_0800274CD854
 
 #include <array>
+
 #include "../scalar_traits.hpp"
 #include "../vector_base.hpp"
 
-namespace dicek
-{
-    namespace math
-    {
-        namespace linalg
-        {
-            template<unsigned long _DIM, typename _scalar_type, typename _scalar_traits = scalar_traits<_scalar_type>>
-            class vector : public vector_base<vector<_DIM,_scalar_type,_scalar_traits>, _scalar_type, _scalar_traits> {
-            public:
-                static const unsigned long DIM = _DIM;
-                typedef vector_base<vector<_DIM,_scalar_type,_scalar_traits>, _scalar_type, _scalar_traits> vector_base_;
-                typedef typename vector_base_::scalar_traits scalar_traits;
-                typedef typename vector_base_::scalar_type scalar_type;
+namespace dicek {
+namespace math {
+namespace linalg {
+template<unsigned long _DIM, typename _scalar_type, typename _scalar_traits = scalar_traits<_scalar_type>>
+class vector : public vector_base<vector<_DIM, _scalar_type, _scalar_traits>, _scalar_type, _scalar_traits> {
+ public:
+  static const unsigned long DIM = _DIM;
+  typedef vector_base<vector<_DIM, _scalar_type, _scalar_traits>, _scalar_type, _scalar_traits> vector_base_;
+  typedef typename vector_base_::scalar_traits scalar_traits;
+  typedef typename vector_base_::scalar_type scalar_type;
 
-                vector()
-                    : m_elm()
-                {
-                }
+  vector() : m_elm() {}
 
-                scalar_type& at( unsigned long index )
-                {
-                    return m_elm.at( index );
-                }
+  scalar_type& at(unsigned long index) {
+    return m_elm.at(index);
+  }
 
-                const scalar_type& at( unsigned long index ) const
-                {
-                    return m_elm.at( index );
-                }
+  const scalar_type& at(unsigned long index) const {
+    return m_elm.at(index);
+  }
 
-                scalar_type& operator[]( unsigned long index )
-                {
-                    return m_elm[ index ];
-                }
+  scalar_type& operator[](unsigned long index) {
+    return m_elm[index];
+  }
 
-                const scalar_type& operator[]( unsigned long index ) const
-                {
-                    return m_elm[ index ];
-                }
+  const scalar_type& operator[](unsigned long index) const {
+    return m_elm[index];
+  }
 
-                template< typename F >
-                vector map( F f ) const
-                {
-                    vector ret;
+  template<typename F>
+  vector map(F f) const {
+    vector ret;
 
-                    for( unsigned long index = 0; index < DIM; ++index ) {
-                        ret[ index ] = f( ( *this )[ index ] );
-                    }
-
-                    return ret;
-                }
-
-            private:
-                std::array<scalar_type, DIM> m_elm;
-
-                vector add_impl( const vector& rhs ) const
-                {
-                    vector ret;
-
-                    for( unsigned long index = 0; index < DIM; ++index ) {
-                        ret[ index ] = ( *this )[ index ] + rhs[ index ];
-                    }
-
-                    return ret;
-                }
-
-                vector scale_impl( scalar_type val ) const
-                {
-                    return map( [ val ]( scalar_type x ) { return val * x; } );
-                }
-
-                friend vector_base_;
-            };
-
-            template< typename V >
-            typename V::scalar_type inner_product( const V& lhs, const V& rhs )
-            {
-                typedef typename V::scalar_type scalar_type;
-                scalar_type ret = scalar_type();
-
-                for( unsigned long index = 0; index < V::DIM; ++index ) {
-                    ret += lhs[ index ] * V::scalar_traits::conj( rhs[ index ] );
-                }
-
-                return ret;
-            }
-
-            template< typename V, typename scalar >
-            scalar norm( const V& v, scalar p )
-            {
-                using std::pow;
-                if( p < scalar( 1 ) ) {
-                    throw std::range_error( "p must be greater than or equal to 1" );
-                }
-
-                typedef typename V::scalar_traits scalar_traits;
-
-                scalar ret = 0;
-                for( unsigned long index = 0; index < V::DIM; ++index ) {
-                    ret += pow( scalar_traits::abs( v[ index ] ), p );
-                }
-
-                return pow( ret, scalar( 1 ) / p );
-            }
-        }
+    for (unsigned long index = 0; index < DIM; ++index) {
+      ret[index] = f((*this)[index]);
     }
+
+    return ret;
+  }
+
+ private:
+  std::array<scalar_type, DIM> m_elm;
+
+  vector add_impl(const vector& rhs) const {
+    vector ret;
+
+    for (unsigned long index = 0; index < DIM; ++index) {
+      ret[index] = (*this)[index] + rhs[index];
+    }
+
+    return ret;
+  }
+
+  vector scale_impl(scalar_type val) const {
+    return map([val](scalar_type x) { return val * x; });
+  }
+
+  friend vector_base_;
+};
+
+template<typename V>
+typename V::scalar_type inner_product(const V& lhs, const V& rhs) {
+  typedef typename V::scalar_type scalar_type;
+  scalar_type ret = scalar_type();
+
+  for (unsigned long index = 0; index < V::DIM; ++index) {
+    ret += lhs[index] * V::scalar_traits::conj(rhs[index]);
+  }
+
+  return ret;
 }
+
+template<typename V, typename scalar>
+scalar norm(const V& v, scalar p) {
+  using std::pow;
+  if (p < scalar(1)) {
+    throw std::range_error("p must be greater than or equal to 1");
+  }
+
+  typedef typename V::scalar_traits scalar_traits;
+
+  scalar ret = 0;
+  for (unsigned long index = 0; index < V::DIM; ++index) {
+    ret += pow(scalar_traits::abs(v[index]), p);
+  }
+
+  return pow(ret, scalar(1) / p);
+}
+}  // namespace linalg
+}  // namespace math
+}  // namespace dicek
 
 #endif /* UUID_E2494444_AF31_11E6_9A65_0800274CD854 */
--- include/dicek/vector_base.hpp
+++ include/dicek/vector_base.hpp
@@ -26,38 +26,32 @@ SOFTWARE.
 
 #include "scalar_traits.hpp"
 
-namespace dicek
-{
-    namespace math
-    {
-        /* Curiously Recurring Template Pattern */
-        template<typename derived, typename _scalar_type, typename _scalar_traits = scalar_traits<_scalar_type>>
-        class vector_base
-        {
-        public:
-          typedef _scalar_traits scalar_traits;
-          typedef typename scalar_traits::type scalar_type;
+namespace dicek {
+namespace math {
+/* Curiously Recurring Template Pattern */
+template<typename derived, typename _scalar_type, typename _scalar_traits = scalar_traits<_scalar_type>>
+class vector_base {
+ public:
+  typedef _scalar_traits scalar_traits;
+  typedef typename scalar_traits::type scalar_type;
 
-          derived add( const derived& rhs ) const
-          {
-              const derived* this_ = static_cast<const derived*>( this );
-              return this_->add_impl( rhs );
-          }
+  derived add(const derived& rhs) const {
+    const derived* this_ = static_cast<const derived*>(this);
+    return this_->add_impl(rhs);
+  }
 
-          derived operator+( const derived& rhs ) const
-          {
-              return add( rhs );
-          }
+  derived operator+(const derived& rhs) const {
+    return add(rhs);
+  }
 
-          derived scale( scalar_type val ) const
-          {
-              const derived* this_ = static_cast<const derived*>( this );
-              return this_->scale_impl( val );
-          }
+  derived scale(scalar_type val) const {
+    const derived* this_ = static_cast<const derived*>(this);
+    return this_->scale_impl(val);
+  }
 
-        private:
-        };
-    }
-}
+ private:
+};
+}  // namespace math
+}  // namespace dicek
 
 #endif /* UUID_23E3CA5E_8877_11E7_86C9_10050143A0C0 */
