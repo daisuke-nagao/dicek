de72852e786a33ef1e8de70caa7d281c1daa61b5
Daisuke Nagao <daisuke-nagao@users.noreply.github.com>
2022-02-14 17:46:17 -0800

    Refactoring test

--- test/vectorTest.cpp
+++ test/vectorTest.cpp
@@ -101,8 +101,6 @@ TEST(vectorTest, external_buffer_constructor) {
     EXPECT_EQ(check.at(i), cvec5.at(i));
   }
 
-  const type::scalar_type* cdata = cvec5.data();
-  EXPECT_EQ(buf.data(), cdata);
-  type::scalar_type* data = vec5.data();
-  EXPECT_EQ(buf.data(), data);
+  EXPECT_EQ(buf.data(), cvec5.data());
+  EXPECT_EQ(buf.data(), vec5.data());
 }
